909d477addaa3bb313ee8acaf9fea0290be9b069
negra1 <72291015+negra1@users.noreply.github.com>
2020-10-03 01:38:27 -0300

    Initial commit

---
+++ lesliecatdog/apps/usuario/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Usuario(models.Model):
+    name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
